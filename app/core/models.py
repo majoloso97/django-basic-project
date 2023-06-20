c24e6d3c04bad05c18c5f54e76fb82cc6c63032a
majoloso97 <majoloso97@gmail.com>
2023-06-20 10:02:15 -0600

    Add calls model and serializer

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UpcomingCall(models.Model):
+    """Upcoming Calls Model"""
+    call_name = models.CharField(
+        "Call Name", max_length=255, help_text="Name of the upcoming call")
+    user = models.ForeignKey(User, related_name='+', on_delete=models.CASCADE,
+                             help_text="User that created the upcoming call")
+    participants = models.ManyToManyField(
+        User, related_name='+', help_text="Users attending the upcoming call")
+    timestamp = models.BigIntegerField()
+    meeting_link = models.CharField(
+        max_length=500, default='', help_text="Meeting Link (Zoom)")
+    recours_weekly = models.BooleanField(
+        default=False, help_text="Determine if the upcoming call should be recurring for every week")
+
+    def __str__(self):
+        return self.call_name
--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from models import UpcomingCall
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,3 +18,22 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['name']
+
+
+class UpcomingCallSerializer(serializers.ModelSerializer):
+    """Upcoming Call Serializer."""
+
+    class Meta:
+        model = UpcomingCall
+        fields = [ 'id', 'call_name', 'timestamp', 'participants', 'meeting_link', 'recours_weekly' ]
+
+
+    def validate(self, data):
+        """Check that at least one participant is required."""
+        try:
+            if len(data['participants']) == 0:
+                raise serializers.ValidationError("At least one participant is required")
+            return data 
+        except KeyError as ex:
+            raise serializers.ValidationError("At least one participant is required")
+           
