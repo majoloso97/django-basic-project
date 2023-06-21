c15bc26c0c7b2c1d095a6105629a8fd1a12d0ec2
0xlaguna <maykol.laguna@nicasource.com>
2023-06-21 01:30:35 -0600

    django-compatible folder structure

--- src/app/urls.py
+++ src/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .core import views
+from core import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
