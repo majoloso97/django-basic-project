82ddf1c9ffbbe2f47ad5fced086198f94e18d622
majoloso97 <majoloso97@gmail.com>
2023-06-21 09:24:23 -0600

    Add models to django admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import AdditionalUser, UpcomingCall
 
 # Register your models here.
+
+@admin.register(AdditionalUser)
+class AdditionalUserAdmin(admin.ModelAdmin):
+    list_display = ('timezone', )
+    list_filter = ('user',)
+
+@admin.register(UpcomingCall)
+class UpcomingCallAdmin(admin.ModelAdmin):
+    list_display = ('call_name', 'user', 'timestamp')
+    list_filter = ('user',)
